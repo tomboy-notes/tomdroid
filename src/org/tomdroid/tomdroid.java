175d2783defd3c089b2baa24a720df4e50d2c222
Olivier Bilodeau <olivier@bottomlesspit.org>
2008-11-19 02:01:14 -0500

    A string and a button! Yay!

---
+++ src/org/tomdroid/tomdroid.java
@@ -0,0 +1,17 @@
+package org.tomdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class tomdroid extends Activity {
+	
+    /** Called when the activity is created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.main);
+    }
+    
+    
+}
