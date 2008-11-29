6bd55182f60103fa0506eabfccd3c447376b1293
Olivier Bilodeau <olivier@bottomlesspit.org>
2008-11-29 13:01:50 -0500

    Added a scrollview around the NoteView activity UI so we can now vertically scroll throught the text of big notes.

--- src/org/tomdroid/R.java
+++ src/org/tomdroid/R.java
@@ -15,8 +15,9 @@ public final class R {
     }
     public static final class id {
         public static final int btnURL=0x7f050002;
-        public static final int content=0x7f050003;
+        public static final int content=0x7f050004;
         public static final int strURL=0x7f050000;
+        public static final int textScroller=0x7f050003;
         public static final int txtURL=0x7f050001;
     }
     public static final class layout {
