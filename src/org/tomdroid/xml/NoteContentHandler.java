826d92cab88b62cf178cccfcfd5015b9949efba6
Olivier Bilodeau <olivier@bottomlesspit.org>
2010-02-01 00:11:47 -0500

    Fixed parsing issues related to a misuse of the XML parser. It closes bug #483101 and is kinda related to #364850 which I just fixed a few hours ago. To be honest, I feel like I probably introduced a bunch of other bugs with people doing weird nesting but heck I'll fix them as they show up ;)

--- src/org/tomdroid/xml/NoteContentHandler.java
+++ src/org/tomdroid/xml/NoteContentHandler.java
@@ -22,6 +22,8 @@
  */
 package org.tomdroid.xml;
 
+import java.util.ArrayList;
+
 import org.tomdroid.Note;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -71,7 +73,27 @@ public class NoteContentHandler extends DefaultHandler {
 	private final static String LIST = "list";
 	private final static String LIST_ITEM = "list-item";
 	
-	// accumulate notecontent is this var since it spans multiple xml tags
+	// holding state for tags
+	private int boldStartPos;
+	private int boldEndPos;
+	private int italicStartPos;
+	private int italicEndPos;
+	private int strikethroughStartPos;
+	private int strikethroughEndPos;
+	private int highlightStartPos;
+	private int highlightEndPos;
+	private int monospaceStartPos;
+	private int monospaceEndPos;
+	private int smallStartPos;
+	private int smallEndPos;
+	private int largeStartPos;
+	private int largeEndPos;
+	private int hugeStartPos;
+	private int hugeEndPos;
+	private ArrayList<Integer> listItemStartPos = new ArrayList<Integer>(0);
+	private ArrayList<Integer> listItemEndPos = new ArrayList<Integer>(0);
+	
+	// accumulate note-content in this var since it spans multiple xml tags
 	private SpannableStringBuilder ssb;
 	
 	public NoteContentHandler(SpannableStringBuilder noteContent) {
@@ -129,24 +151,71 @@ public class NoteContentHandler extends DefaultHandler {
 		if (inNoteContentTag) {
 			if (name.equals(BOLD)) {
 				inBoldTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), boldStartPos, boldEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				boldStartPos = 0;
+				boldEndPos = 0;
+
 			} else if (name.equals(ITALIC)) {
 				inItalicTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), italicStartPos, italicEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				italicStartPos = 0;
+				italicEndPos = 0;
+
 			} else if (name.equals(STRIKETHROUGH)) {
 				inStrikeTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new StrikethroughSpan(), strikethroughStartPos, strikethroughEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				strikethroughStartPos = 0;
+				strikethroughEndPos = 0;
+
 			} else if (name.equals(HIGHLIGHT)) {
 				inHighlighTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new BackgroundColorSpan(Note.NOTE_HIGHLIGHT_COLOR), highlightStartPos, highlightEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				highlightStartPos = 0;
+				highlightEndPos = 0;
+				
 			} else if (name.equals(MONOSPACE)) {
 				inMonospaceTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new TypefaceSpan(Note.NOTE_MONOSPACE_TYPEFACE), monospaceStartPos, monospaceEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				monospaceStartPos = 0;
+				monospaceEndPos = 0;
+
 			} else if (name.equals(SMALL)) {
 				inSizeSmallTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_SMALL_FACTOR), smallStartPos, smallEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				smallStartPos = 0;
+				smallEndPos = 0;
+				
 			} else if (name.equals(LARGE)) {
 				inSizeLargeTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_LARGE_FACTOR), largeStartPos, largeEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				largeStartPos = 0;
+				largeEndPos = 0;
+
 			} else if (name.equals(HUGE)) {
 				inSizeHugeTag = false;
+				// apply style and reset position keepers
+				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_HUGE_FACTOR), hugeStartPos, hugeEndPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				hugeStartPos = 0;
+				hugeEndPos = 0;
+
 			} else if (name.equals(LIST)) {
 				inListLevel--;
 			} else if (name.equals(LIST_ITEM)) {
+				// here, we apply margin and create a bullet span. Plus, we need to reset position keepers.
 				inListItem = false;
+				// TODO new sexier bullets?
+				// Show a leading margin that is as wide as the nested level we are in
+				ssb.setSpan(new LeadingMarginSpan.Standard(10*inListLevel), listItemStartPos.get(inListLevel-1), listItemEndPos.get(inListLevel-1), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				ssb.setSpan(new BulletSpan(), listItemStartPos.get(inListLevel-1), listItemEndPos.get(inListLevel-1), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				listItemStartPos.set(inListLevel-1, new Integer(0));
+				listItemEndPos.set(inListLevel-1, new Integer(0));
 			}
 		}
 	}
@@ -166,34 +235,86 @@ public class NoteContentHandler extends DefaultHandler {
 			// apply style if required
 			// TODO I haven't tested nested tags yet
 			if (inBoldTag) {
-				ssb.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (boldStartPos == 0) {
+					boldStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				boldEndPos = strLenEnd;
 			}
 			if (inItalicTag) {
-				ssb.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (italicStartPos == 0) {
+					italicStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				italicEndPos = strLenEnd;
 			}
 			if (inStrikeTag) {
-				ssb.setSpan(new StrikethroughSpan(), strLenStart, ssb.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (strikethroughStartPos == 0) {
+					strikethroughStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				strikethroughEndPos = strLenEnd;
 			}
 			if (inHighlighTag) {
-				ssb.setSpan(new BackgroundColorSpan(Note.NOTE_HIGHLIGHT_COLOR), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (highlightStartPos == 0) {
+					highlightStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				highlightEndPos = strLenEnd;
 			}
 			if (inMonospaceTag) {
-				ssb.setSpan(new TypefaceSpan(Note.NOTE_MONOSPACE_TYPEFACE), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (monospaceStartPos == 0) {
+					monospaceStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				monospaceEndPos = strLenEnd;
 			}
 			if (inSizeSmallTag) {
-				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_SMALL_FACTOR), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (smallStartPos == 0) {
+					smallStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				smallEndPos = strLenEnd;
 			}
 			if (inSizeLargeTag) {
-				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_LARGE_FACTOR), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (largeStartPos == 0) {
+					largeStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				largeEndPos = strLenEnd;
 			}
 			if (inSizeHugeTag) {
-				ssb.setSpan(new RelativeSizeSpan(Note.NOTE_SIZE_HUGE_FACTOR), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				// if tag is not equal to 0 then we are already in it: no need to reset it's position again 
+				if (hugeStartPos == 0) {
+					hugeStartPos = strLenStart;
+				}
+				// no matter what, if we are still in the tag, end is now further
+				hugeEndPos = strLenEnd;
 			}
 			if (inListItem) {
-				// TODO new sexier bullets?
-				// Show a leading margin that is as wide as the nested level we are in
-				ssb.setSpan(new LeadingMarginSpan.Standard(10*inListLevel), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-				ssb.setSpan(new BulletSpan(), strLenStart, strLenEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+				// Book keeping of where the list-items started and where they end
+				// we need to do that because characters() can be called several times for the same tag
+				
+				// if listItem's position not already in tracking array, add it. Otherwise if it equals 0 then set
+				if (listItemStartPos.size() < inListLevel) {
+					listItemStartPos.add(new Integer(strLenStart));
+				} else if (listItemStartPos.get(inListLevel-1) == 0) {
+					listItemStartPos.set(inListLevel-1, new Integer(strLenStart));					
+				}
+				// no matter what, we track the end (we add if array not big enough or set otherwise) 
+				if (listItemEndPos.size() < inListLevel) {
+					listItemEndPos.add(new Integer(strLenEnd));
+				} else {
+					listItemEndPos.set(inListLevel-1, strLenEnd);					
+				}
 			}
 		}
 	}
